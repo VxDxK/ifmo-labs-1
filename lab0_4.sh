82d02fdfafd1f4e0bd290593e8123a74e067b4e4
vadim <vadim.v.ponomarev@gmail.com>
2021-10-31 20:05:51 +0300

    С днем апдейта

--- lab0_4.sh
+++ lab0_4.sh
@@ -1,8 +1,7 @@
 cd lab0
-wc -m linoone9 >> linoone9
-ls -alR 2> /dev/null| grep "r$" | sort -r -k9
+wc -m linoone9 2>/dev/null >> linoone9
+ls -lR | grep -v "^\." | grep -v "^total" | grep -v "^$" | grep "r$" | sort -r -k9
 cat -n rhyhorn9 | sort -r -k2
 cat -n echo hitmontop8/* 2> /tmp/opd | grep "on"
 cat -n echo aron3/* 2> /dev/null | grep -v "gon"
-ls -lR | grep "ron" | tail -n 2
-ls -lR | grep -v "^\." | grep -v "^total" | grep -v "^$" | tail
+ls -tlR | grep -v "^\." | grep -v "^total" | grep -v "^$" | grep "ron" | tail -2
