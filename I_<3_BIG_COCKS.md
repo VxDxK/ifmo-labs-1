fb8111e1c8318c92ed70380be39540489fa1ac52
vadim <vadim.v.ponomarev@gmail.com>
2023-05-09 22:28:03 +0300

    mef

---
+++ I_<3_BIG_COCKS.md
@@ -0,0 +1,3 @@
+```bash
+mvn dependency:copy-dependencies
+```
---
+++ src/test/java/com/vdk/lab3aaaaaaaa/MonadkaTest.java
@@ -0,0 +1,60 @@
+package com.vdk.lab3aaaaaaaa;
+
+//import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MonadkaTest {
+
+    public MonadkaTest() {
+    }
+
+    @Test
+    public void check() {
+        Monadka<Double> mon = Monadka.of(5d);
+        mon.check(x -> x > 3, "X should be greater 5");
+        assertDoesNotThrow(() -> {
+            mon.get();
+        });
+
+        mon.check(x -> x > 10, "X should be greater 10").check(x -> x > 11, "X should be greater 11");
+        assertThrows(RuntimeException.class, mon::get);
+        try {
+            mon.get();
+        } catch (RuntimeException e) {
+            assertEquals(e.getSuppressed().length, 2);
+        }
+    }
+
+    @Test
+    void checkFinal() {
+        Monadka<Double> mon = Monadka.of(5d);
+        mon.checkFinal(x -> x > 3, "X should be greater 5");
+        assertDoesNotThrow(() -> {
+            mon.get();
+        });
+
+        mon.checkFinal(x -> x > 10, "X should be greater 10").checkFinal(x -> x > 11, "X should be greater 11");
+        assertThrows(RuntimeException.class, mon::get);
+        try {
+            mon.get();
+        } catch (RuntimeException e) {
+            assertEquals(e.getSuppressed().length, 1);
+        }
+    }
+
+    @Test
+    void map() {
+        Monadka<Double> monadka = Monadka.of("5.034").map(Double::parseDouble).check(x -> x > 0, "");
+
+        assertDoesNotThrow(() -> {
+            monadka.get();
+        });
+        assertEquals(5.034, monadka.get());
+
+        Monadka<Double> mon = Monadka.of("5.0ad34").map(Double::parseDouble).check(x -> x > 0, "");
+        assertThrows(RuntimeException.class, mon::get);
+    }
+}
